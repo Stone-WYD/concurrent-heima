683aecc0488dd0b3f1d2d56c7f5c6a75a3a8be96
王玉东 <1174872613@qq.com>
2023-04-07 13:34:55 +0800

    加入一些对偏向锁的学习和理解

--- case_java8/src/main/java/cn/itcast/n4/TestBiased.java
+++ case_java8/src/main/java/cn/itcast/n4/TestBiased.java
@@ -128,6 +128,8 @@ public class TestBiased {
         Thread t1 = new Thread(() -> {
             synchronized (d) {
                 log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+                //  log.debug("d的hashcode：{}", d.hashCode());
+                //  log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
             }
             synchronized (TestBiased.class) {
                 TestBiased.class.notify();
@@ -135,7 +137,7 @@ public class TestBiased {
         }, "t1");
         t1.start();
 
-
+        // log.debug("d的hashcode：{}", d.hashCode());
         Thread t2 = new Thread(() -> {
             synchronized (TestBiased.class) {
                 try {
@@ -146,14 +148,18 @@ public class TestBiased {
             }
             log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
             synchronized (d) {
+                // log.debug("d的hashcode：{}", d.hashCode());
                 log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
             }
             log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
         }, "t2");
         t2.start();
+
+        // 1. 加锁(偏向锁，轻量级锁)期间如果要求 hashcode，则锁直接变成重量级锁
+        // 2. 已上偏向锁，但锁对象没被锁住的时候求 hashcode，偏向锁会被撤销，变成轻量级锁
+        // 3. 没有其他线程时偏向锁可以提高性能，一旦出现其他线程使用同一个锁，偏向锁就会撤销变成轻量级锁
+        // 4. 如果偏向锁还处于使用中就被其他线程访问，那样竞争线程会进入阻塞，直至运行至安全点偏向锁被撤销然后变成重量级锁
     }
-
-
 
     // 测试偏向锁
     private static void test1() {
@@ -166,6 +172,67 @@ public class TestBiased {
             e.printStackTrace();
         }
         log.debug(ClassLayout.parseInstance(new Dog()).toPrintableSimple());
+    }
+
+    private static void mytest1() throws InterruptedException {
+        Dog d = new Dog();
+        Thread t1 = new Thread(() -> {
+            log.debug("t1运行中, t1的threadId: {}", Thread.currentThread().getId() );
+            log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+            synchronized (d) {
+                log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+            }
+        }, "t1");
+        t1.start();
+
+        t1.join();
+        log.debug("主线程等待t1运行完，准备启动t2运行...");
+        log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+        Thread t2 = new Thread(() -> {
+            log.debug("t2运行中, t2的threadId: {}", Thread.currentThread().getId() );
+            log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+            synchronized (d) {
+                log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+            }
+            log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+        }, "t2");
+        t2.start();
+
+        // 分析结果可知，偏向锁锁在一个对象上后，即使线程结束了，该锁也没有被去除。即该过程是不可逆的。
+    }
+
+    private static void mytest2(){
+        Dog d = new Dog();
+        Thread t1 = new Thread(() -> {
+            log.debug("t1运行中, t1的threadId: {}", Thread.currentThread().getId() );
+            log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+            synchronized (d) {
+                log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+                try {
+                    Thread.sleep(7000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            log.debug("睡眠后的偏向锁状态...");
+            log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+        }, "t1");
+        t1.start();
+
+        Thread t2 = new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            log.debug("t2运行中, t2的threadId: {}", Thread.currentThread().getId() );
+            log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+            synchronized (d) {
+                log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+            }
+            log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
+        }, "t2");
+        t2.start();
     }
 }
 
